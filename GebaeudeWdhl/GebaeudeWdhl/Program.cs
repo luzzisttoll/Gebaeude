e3c2fb2c0379c0014c4906c2979a7a30a4489d0b
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2022-09-16 14:10:51 +0200

    hell yeah

---
+++ GebaeudeWdhl/GebaeudeWdhl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiederholung
+{
+    public class gebaeude
+    {
+        string Name; // Attribut der Klasse Gebäude
+        public string NameAccessible {
+
+            get { return Name; }
+
+            set { Name = value; }
+
+        }
+        public int gebaeudeflaeche
+        {
+            get { return gebaeudeflaeche; }
+            set { gebaeudeflaeche = value; }
+        }
+    }
+}
